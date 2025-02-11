d3a51b2742d356ac7097ae40739b37be56cf6673
stefan <stefan.holmberg@systementor.se>
2025-02-11 16:06:03 +0100

    first commit

---
+++ employee/employee.go
@@ -0,0 +1,9 @@
+package employee
+
+type Employee struct {
+	Name          string // mandatory
+	Age           int    // mandatory
+	StreetAddress string
+	PostalCode    int
+	City          string
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module systementor.se/godemo0211
+
+go 1.23.1
